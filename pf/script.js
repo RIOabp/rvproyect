b8f0ebd4792d207a343d10b756a881ec003db2d1
RIOabp <williambaculima3@gmail.com>
2024-02-08 16:22:47 +0100

    Update script.js

--- pf/script.js
+++ pf/script.js
@@ -10,7 +10,7 @@ var swiper = new Swiper(".blog-slider", {
         invert: false,
     },
     autoplay: {
-        delay: 3500,
+        delay: 4500,
     },
     breakpoints: {
         0: {
@@ -34,3 +34,58 @@ var swiper = new Swiper(".blog-slider", {
   
   });
   
+
+/*-------------------------------next and back -----------------*/
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let current = 1; // Variable para rastrear el div actual mostrado
+
+    // Función para mostrar los divs en el rango especificado
+    function showDivs(start, end) {
+        const container = document.querySelector('.containerc');
+        const divs = container.querySelectorAll('div[id^="p"]');
+        divs.forEach((div, index) => {
+            if (index + 1 >= start && index + 1 <= end) {
+                div.style.display = "block"; // Muestra el div si está en el rango
+            } else {
+                div.style.display = "none"; // Oculta el div si está fuera del rango
+            }
+        });
+    }
+
+    // Mostrar los primeros 10 divs al cargar la página
+    showDivs(1, 15);
+
+    // Botón Siguiente
+    document.getElementById("next").addEventListener("click", function() {
+        if (current + 15 <= 20) { // Si hay al menos 15 divs más para mostrar
+            current += 15; // Actualiza la posición actual
+            showDivs(current, current + 9); // Muestra los siguientes 10 divs
+        }
+    });
+
+    // Botón Anterior
+    document.getElementById("back").addEventListener("click", function() {
+        if (current - 15 >= 1) { // Si hay al menos 10 divs anteriores para mostrar
+            current -= 15; // Actualiza la posición actual
+            showDivs(current, current + 14); // Muestra los 10 divs anteriores
+        }
+    });
+});
+
+
+
+// ------------------------------- FOOTER------------------------
+
+
+
+
+
+
+
+
+
+
+
+
